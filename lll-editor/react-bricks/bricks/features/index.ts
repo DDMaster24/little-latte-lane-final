74817814a374816663b3c227f1eb3b06d53b285f
DDMaster24 <dariusschutte124@gmail.com>
2025-09-14 21:34:38 +0200

    CLEAN SLATE: Complete removal of React Bricks and page editor
    - Reverted to checkpoint ba88e4c (pre-React Bricks state)
    - Removed all React Bricks environment variables
    - Deleted lll-editor directory (previous test project)
    - No page editor references in admin dashboard
    - TypeScript compilation clean
    - Build process working perfectly
    - Ready for fresh React Bricks integration attempt

--- lll-editor/react-bricks/bricks/features/index.ts
+++
@@ -1,7 +0,0 @@
-import { types } from 'react-bricks/rsc'
-import Features from './Features'
-import FeatureItem from './FeatureItem'
-
-const features: types.Brick<any>[] = [Features, FeatureItem]
-
-export default features
--- lll-editor/react-bricks/bricks/index.ts
+++
@@ -1,9 +0,0 @@
-import { types } from 'react-bricks/rsc'
-
-import HeroUnit from './HeroUnit'
-import features from './features'
-import layout from './layout'
-
-const bricks: types.Brick<any>[] = [HeroUnit, ...layout, ...features]
-
-export default bricks
