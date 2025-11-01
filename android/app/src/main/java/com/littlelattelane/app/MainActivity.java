6d3d7d723371e6679ffd3a40adfe428498340865
DDMaster24 <dariusschutte124@gmail.com>
2025-11-01 18:17:47 +0200

    fix: change onResume() access modifier to public in MainActivity
    Fix compilation error: onResume() must be public to match BridgeActivity signature

--- android/app/src/main/java/com/littlelattelane/app/MainActivity.java
+++ android/app/src/main/java/com/littlelattelane/app/MainActivity.java
@@ -92,7 +92,7 @@ public class MainActivity extends BridgeActivity {
     }
     
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         Log.d(TAG, "📱 MainActivity resumed - ready to handle deep links");
     }
